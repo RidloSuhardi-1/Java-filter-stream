c5941e5e64e212a683ffc0ac3710e0ada1ea8988
Ridlo Shuhardi <62533910+RidloSuhardi-1@users.noreply.github.com>
2021-10-10 23:10:24 +0700

    Pembacaan text/string

--- src/praktikum1/ReadInputKeyboard.java
+++ src/praktikum1/ReadInputKeyboard.java
@@ -3,6 +3,7 @@ package praktikum1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 /**
@@ -12,15 +13,19 @@ import java.util.logging.Logger;
 public class ReadInputKeyboard {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
         System.out.println("Masukan karakter apapun, [tekan keluar tekan `q`]: ");
-        char input = 0;
+        // char input = 0;
+        String input = "";
         do {
              try {
-                input = (char) br.read();
-                System.out.print("" + input);
+                // input = (char) br.read();
+                // input = sc.nextLine();
+                input = br.readLine();
+                System.out.print("" + input + "\n");
              } catch (IOException ex) {
                 Logger.getLogger(ReadInputKeyboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
              }
-         } while (input != 'q');
+         } while (input != "q");
     }
 }
